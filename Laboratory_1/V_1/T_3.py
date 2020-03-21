bf638f9079c663ab4bea579c4bc367ebf297c1b0
Drobyazko <philipjava1111@gmail.com>
2020-03-21 18:18:31 +0200

    T_3.1

--- Laboratory_1/V_1/T_3.py
+++ Laboratory_1/V_1/T_3.py
@@ -1,6 +1,6 @@
 """
 Катети прямокутного трикутника уводяться з клавіатури. Обчислити довжину гіпотенузи, периметр і площу цього трикутника. Відповідь дати з точністю до 10 знаків після коми.
-The cattails of the direct tricot are led away from the keyboard. Calculate the pre-dozen gіpotenuzi, the perimeter and the area of ​​the whole trick. Departure to the event up to 10 days a week.
+The rectangular triangle catheters are entered from the keyboard. Calculate the length of the hypotenuse, the perimeter, and the area of this triangle. Answer up to 10 decimal places.
 """
 from math import sqrt
 
@@ -22,10 +22,10 @@ def correct(x):
 def main():
     a = int(in_put_for_a())
     b = int(in_put_for_b())
-    gip = round(sqrt(a * a + b * b),10)
+    hyp = round(sqrt(a * a + b * b),10)
     perimeter = round(gip + a + b,10)
     area = round((a * b)/2,10)
-    return print("Гипотенуза "+str(gip)+"\n"+"Периметр "+str(perimeter)+"\n"+"Площадь "+str(area))
+    return print("Гипотенуза "+str(hyp)+"\n"+"Периметр "+str(perimeter)+"\n"+"Площадь "+str(area))
 
 non = 1
 while non != 0:
