a4dff14c88dc2e577247cbc5001a48cbc96c78fc
Drobyazko <philipjava1111@gmail.com>
2020-03-21 18:18:46 +0200

    T_4.0

---
+++ Laboratory_1/V_1/T_4.py
@@ -0,0 +1,5 @@
+"""
+Користувач уводить три числа. Збільшити перше число в два рази, друге числа зменшити на 3, третє число звести в квадрат і потім знайти суму нових трьох чисел.
+The user enters three numbers. Increase the first number twice, reduce the second numbers by 3, reduce the third number to a square and then find the sum of the new three numbers.
+"""
+
