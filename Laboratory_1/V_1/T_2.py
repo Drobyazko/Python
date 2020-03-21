704bb284986dbf686c0daab49cc33fce7603f19a
Drobyazko <philipjava1111@gmail.com>
2020-03-21 17:40:10 +0200

    T_2.0

---
+++ Laboratory_1/V_1/T_2.py
@@ -0,0 +1,5 @@
+"""
+Дійсні числа a і b уводяться з клавіатури. Для a обчислити відсоток b від цього числа.
+The numbers a і b are taken away from the keyboard. For a, calculate the number b of the whole number.
+"""
+
